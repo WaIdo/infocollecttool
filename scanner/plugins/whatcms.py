4965b4adcedf00bc6bd531b095aa3084184a3d78
Waldo <Waldo@Waldos-MacBook-Air.local>
2018-11-23 15:23:32 +0800

    初始化提交

---
+++ scanner/plugins/whatcms.py
@@ -0,0 +1,23 @@
+import requests
+import json
+import time
+
+class gwhatweb:
+    def __init__(self,url):
+        self.url = url
+        self.time=0
+
+    def whatweb(self):
+        url = 'http://whatweb.bugscaner.com/what/'
+        start = time.clock()
+        headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:56.0) Gecko/20100101 Firefox/56.0','Referer':'http://whatweb.bugscaner.com/look/'}
+        cocokies = {'saeut': 'CkMPHlqbqdBQWl9NBG+uAg=='}
+        new_url = self.url.strip('/').replace('http://','').replace('https://','')
+        data = {'url': new_url, 'hash': '0eca8914342fc63f5a2ef5246b7a3b14_7289fd8cf7f420f594ac165e475f1479'}
+        content = json.loads(requests.post(url,headers=headers,data=data).text)
+        end = time.clock()
+        self.time = end - start
+        if content['cms']:
+            return {'total':1424,'url':self.url,'result':content['cms'],'time':'%.3f s' % self.time}
+        else:
+            return {'total':1424,'url':self.url,'result':'未知CMS','time':'%.3f s' % self.time}
